ba0de0352d30678fb3561b3683e9021bfd4e16b4
mrossisei <mrossiseifert@itba.edu.ar>
2025-10-12 21:03:40 -0300

    Actualización enums

--- src/main/java/cli/CryptoMode.java
+++ src/main/java/cli/CryptoMode.java
@@ -8,4 +8,12 @@ public enum CryptoMode {
     CFB,
     OFB,
     CBC
+
+    public static CryptoMode fromString(String text) {
+        try {
+            return CryptoMode.valueOf(text.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new StegoException("Modo de cifrado no válido: '" + text + "'. Use ecb, cfb, ofb o cbc.");
+        }
+    }
 }
--- src/main/java/cli/OperationMode.java
+++ src/main/java/cli/OperationMode.java
@@ -5,11 +5,4 @@ public enum OperationMode {
     EMBED,
     EXTRACT
 
-    public static CryptoMode fromString(String text) {
-        try {
-            return CryptoMode.valueOf(text.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            throw new StegoException("Modo de cifrado no válido: '" + text + "'. Use ecb, cfb, ofb o cbc.");
-        }
-    }
 }
